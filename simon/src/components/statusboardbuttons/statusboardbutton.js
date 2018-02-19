c1805cb6a316b625e43d6d2817b9b5fd89d4bb15
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-02-18 23:18:24 -0600

    stausboardbutton.js CSS formatting
    correct spacing error in button selector block

--- simon/src/components/statusboardbuttons/statusboardbutton.js
+++ simon/src/components/statusboardbuttons/statusboardbutton.js
@@ -4,17 +4,17 @@ import styled from 'styled-components';
 const ButtonStyledDiv = styled.div`
   display: inline-block;
   button {
-  width: 75px;
-  margin: 0 10px 20px;
-  padding: 10px 0 7px;
-  border-radius: 10px;
-  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
-  -webkit-box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
-  background-color: #fffafa;
-  letter-spacing: 1px;   
-  font-family: Orbitron, sans-serif;
-  font-weight: 600;
-  color: #25292e;
+    width: 75px;
+    margin: 0 10px 20px;
+    padding: 10px 0 7px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.3);
+    background-color: #fffafa;
+    letter-spacing: 1px;   
+    font-family: Orbitron, sans-serif;
+    font-weight: 600;
+    color: #25292e;
   &:hover {
     cursor: pointer;
   }
